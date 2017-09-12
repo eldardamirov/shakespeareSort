6972466105a4df038d4a4a02d2eb5bccccc17446
eldardamirov <eldardamirov75@mail.ru>
2017-09-12 19:07:56 +0300

    Initial Commit

---
+++ oneginSortThreadEdition/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  oneginSortThreadEdition
+//
+//  Created by Эльдар Дамиров on 12.09.17.
+//  Copyright © 2017 Эльдар Дамиров. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
